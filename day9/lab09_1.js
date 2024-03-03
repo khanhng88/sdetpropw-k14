7795070a4841c15474d51f4d18e11c08f0d1cbe1
khanhnguyen <emma.devn@gmail.com>
2024-03-03 20:59:59 +0700

    commit lab 9

--- day9/lab09_1.js
+++ day9/lab09_1.js
@@ -8,34 +8,31 @@ GET	/posts/1/comments
 GET	/comments?postId=1
 
 */
+const readLine = require('readline-sync')
 
 function getAllPosts() {
     const targetURL = 'https://jsonplaceholder.typicode.com/posts'
-
-
     return fetch(targetURL).then(function (response) {
         return response.json()
     })
     
 }
 
-function run(userId , postId) {
-    const value =  getAllPosts()
-    let valueJson =  value.json()
-
-    let arrayObj = valueJson
+function run() {
     let userId = readLine.question('Please input your userid: ')
     let postId = readLine.question('Please input your postid: ')
 
-    const targetPost = arrayObj.filter(function(post) {
-        return post.userId == userId && post.id == postId
-    })
-    if(targetPost.length > 0) {
-        console.log(targetPost);
-    }else{
-        console.log('not found');
-    }
-  
 
+    getAllPosts().then(function(posts) {
+        const targetPost = posts.filter(function(post) {
+            return post.userId == userId && post.id == postId
+        })
+        if(targetPost.length > 0) {
+            console.log(targetPost);
+        }else{
+            console.log('not found');
+        }
+    })
 }
+run()
 
